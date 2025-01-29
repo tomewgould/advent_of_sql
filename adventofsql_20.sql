756c1052c0aa2699c512a34241e92e56d3e2d178
Tom Gould <158174657+tomewgould@users.noreply.github.com>
2025-01-29 15:10:47 +0000

    Update adventofsql_20.sql
    Added comments to help understand code

--- adventofsql_20.sql
+++ adventofsql_20.sql
@@ -1,10 +1,10 @@
 with unnested as (
   select 
     url, 
-    split_part(
-      unnest(
-        string_to_array(
-          split_part(url, '?', 2), 
+    split_part( -- Take the key of each key value pair (separated by =)
+      unnest( -- Parse each element of each array into rows
+        string_to_array( -- Convert string of parameters for each url into an array that is split by each &
+          split_part(url, '?', 2), -- Separate the parameters from the domain
           '&'
         )
       ), 
@@ -14,11 +14,11 @@ with unnested as (
   from 
     web_requests 
   where 
-    url like '%utm_source=advent-of-sql%'
+    url like '%utm_source=advent-of-sql%' -- Filter out url values that do not include desired query parameter
 ) 
 select 
   url, 
-  count(distinct elem) 
+  count(distinct elem) -- Count number of query parameters per url
 from 
   unnested 
 group by 
